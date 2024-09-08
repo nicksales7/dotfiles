beec669aae6a6d3da4108a1330917905c6d9f202
nicksales7 <nicksales72@gmail.com>
2024-09-08 18:44:43 -0400

    updated neovim 2024-09-08

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -37,7 +37,6 @@ vim.g.mkdp_theme = 'light'
 
 -- Dark mode and colorscheme
 vim.opt.background = 'dark'
-vim.cmd.colorscheme('quiet')
 
 -- For nextword idk
 vim.api.nvim_set_keymap('n', '<leader>n', ':noh<CR>', { noremap = true, silent = true })
@@ -141,14 +140,24 @@ vim.opt.swapfile = false
 
 -- Setup lazy.nvim
 require("lazy").setup({
-  spec = {
-    {
+  spec = {{
     {
       "aktersnurra/no-clown-fiesta.nvim", 
       config = function()
         vim.cmd.colorscheme("no-clown-fiesta")
       end,
     },
+      "nvim-treesitter/nvim-treesitter",
+      build = ":TSUpdate",
+      opts = {
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "java", "haskell" },
+        highlight = { enable = true },
+      },
+      config = function(_, opts)
+        require("nvim-treesitter.configs").setup(opts)
+      end,
+    },
+    {
       "nvim-treesitter/nvim-treesitter",
       build = ":TSUpdate",
       opts = {
@@ -185,5 +194,50 @@ require("lazy").setup({
     { "Julian/lean.nvim" },
     { "neovim/nvim-lspconfig" },
     { "nvim-lua/plenary.nvim" },
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' }
+    }
   },
 })
+
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
