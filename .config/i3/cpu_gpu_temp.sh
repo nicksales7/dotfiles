cc642a972974348557fe5587d9ed8e291fc41498
nicksales7 <nicksales72@gmail.com>
2024-09-16 16:57:53 -0400

    Updated 2024-09-16

--- .config/i3/cpu_gpu_temp.sh
+++ .config/i3/cpu_gpu_temp.sh
@@ -1,15 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
-# Get CPU temperature
-cpu_temp=$(sensors | awk '/^temp1:/{print $2}' | sed 's/+//;s/°C//')
+# Adjusting the pattern to match the Tctl temperature for CPU
+cpu_temp=$(sensors | grep 'Tctl' | awk '{print substr($2, 2)}')
+gpu_temp=$(sensors | grep 'edge:' | awk '{print substr($2, 2)}')
 
-# Get GPU temperature
-gpu_temp=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader,nounits | awk '{print $1}')
-
-# Format output
-formatted_cpu_temp=$(printf "%.1f°C" "$cpu_temp")
-formatted_gpu_temp=$(printf "%.1f°C" "$gpu_temp")
-
-# Print formatted temperatures
-echo "CPU/GPU: $formatted_cpu_temp -  $formatted_gpu_temp"
+echo "CPU: $cpu_temp GPU: $gpu_temp"
 
