167feab8c013e36a7faee24d3e0c8d89f9ec14c0
nicksales7 <nicksales72@gmail.com>
2024-10-12 23:04:45 -0400

    Updated 2024-10-12

---
+++ .config/i3/cpu_gpu_temp.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Adjusting the pattern to match the Tctl temperature for CPU
+cpu_temp=$(sensors | grep 'Tctl' | awk '{print substr($2, 2)}')
+gpu_temp=$(sensors | grep 'edge:' | awk '{print substr($2, 2)}')
+
+echo "CPU: $cpu_temp GPU: $gpu_temp"
