46b6f93edaae32059b352d4b6688cae2585c6a82
nicksales7 <nicksales72@gmail.com>
2024-11-09 12:02:07 -0500

    Updated 2024-11-09

--- .config/i3/brightness_control.sh
+++ .config/i3/brightness_control.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
 # Set the desired brightness levels
-LOW_BRIGHTNESS="5%"
-HIGH_BRIGHTNESS="100%"
+LOW_BRIGHTNESS="5"
+HIGH_BRIGHTNESS="100"
 
 # Duration of inactivity in seconds
 INACTIVITY_LIMIT=240
 
 # Initialize current brightness state
 CURRENT_BRIGHTNESS=""
-SLEEP_DURATION=10  # Start with longer sleep time
+SLEEP_DURATION=10  # Start with a longer sleep time
 
-# Get the list of brightness devices
-BRIGHTNESS_DEVICES=("/sys/class/backlight/amdgpu_bl1")
-
+# Main loop
 while true; do
     # Get the idle time in milliseconds
     IDLE_TIME=$(xprintidle)
@@ -21,22 +19,14 @@ while true; do
     if [ "$IDLE_TIME" -ge "$((INACTIVITY_LIMIT * 1000))" ]; then
         # Set low brightness after 4 minutes of inactivity
         if [ "$CURRENT_BRIGHTNESS" != "$LOW_BRIGHTNESS" ]; then
-            for DEVICE in "${BRIGHTNESS_DEVICES[@]}"; do
-                if [ -d "$DEVICE" ]; then
-                    sudo brightnessctl set $LOW_BRIGHTNESS --device="$DEVICE"
-                fi
-            done
+            light -S $LOW_BRIGHTNESS
             CURRENT_BRIGHTNESS=$LOW_BRIGHTNESS
             SLEEP_DURATION=1  # Switch to shorter sleep duration
         fi
     else
         # Set high brightness if there is activity
         if [ "$CURRENT_BRIGHTNESS" != "$HIGH_BRIGHTNESS" ]; then
-            for DEVICE in "${BRIGHTNESS_DEVICES[@]}"; do
-                if [ -d "$DEVICE" ]; then
-                    sudo brightnessctl set $HIGH_BRIGHTNESS --device="$DEVICE"
-                fi
-            done
+            light -S $HIGH_BRIGHTNESS
             CURRENT_BRIGHTNESS=$HIGH_BRIGHTNESS
             SLEEP_DURATION=10  # Switch back to longer sleep duration
         fi
